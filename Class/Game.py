519a35b91f4663b8e278bd7a6a50b02fa6ad9644
History54 <quentinbeyl01@gmail.com>
2023-09-22 14:07:24 +0200

    pepito

--- Class/Game.py
+++ Class/Game.py
@@ -1,19 +1,20 @@
 import random
 
-from Class.Pieces.Piece import *
-from Class.Pieces.Pawn import *
-from Class.Pieces.Rook import *
-from Class.Pieces.Knight import *
 from Class.Pieces.Bishop import *
-from Class.Pieces.Queen import *
 from Class.Pieces.King import *
+from Class.Pieces.Knight import *
+from Class.Pieces.Pawn import *
+from Class.Pieces.Piece import *
+from Class.Pieces.Queen import *
+from Class.Pieces.Rook import *
+
 
 class Game:
 
     def __init__(self):
 
         #création de la matrice d'échiquier
-        self.Board = [
+        self.board = [
             [Rook("Black"), Knight("Black"), Bishop("Black"), Queen("Black"), King("Black"), Bishop("Black"), Knight("Black"), Rook("Black")],
             [Pawn("Black"),Pawn("Black"),Pawn("Black"),Pawn("Black"),Pawn("Black"),Pawn("Black"),Pawn("Black"),Pawn("Black")],
             [None,None,None,None,None,None,None,None],
--- Pages/Game_Page.py
+++ Pages/Game_Page.py
@@ -1,11 +1,12 @@
 from Class.Game import *
-from Class.Pieces.Piece import *
-from Class.Pieces.Pawn import *
-from Class.Pieces.Rook import *
-from Class.Pieces.Knight import *
 from Class.Pieces.Bishop import *
-from Class.Pieces.Queen import *
 from Class.Pieces.King import *
+from Class.Pieces.Knight import *
+from Class.Pieces.Pawn import *
+from Class.Pieces.Piece import *
+from Class.Pieces.Queen import *
+from Class.Pieces.Rook import *
+
 
 def Game_Page(game : Game()):
     case_letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
@@ -16,7 +17,9 @@ def Game_Page(game : Game()):
             moveName = str(input("Move (piece pos + space + new pos): "))
 
             #Remplace le première caractère du coup par sa valeur
-            moveName[0] == case_letters.find(moveName[0])
+            moveName[0] == str(case_letters.index(moveName[0] + 1))
 
-            if moveName[0] in case_letters and moveName[1]:
-                
+            #Verifie si la case est dans la matrice
+            if moveName[0] in case_letters and int(moveName[1]) in range(1, 8):
+                if game.board[int(moveName[0]), int(moveName[1])] != None:
+                    break
