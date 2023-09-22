a1ddfa1eefdf2b2a21a67d03e573f9962affd95c
sylvaneaucode <sylvain.craftmine@gmail.com>
2023-09-22 14:57:20 +0200

    Knight

--- Class/Pieces/Knight.py
+++ Class/Pieces/Knight.py
@@ -10,4 +10,13 @@ class Knight(Piece):
 
     def LegalMoves(self, Board):
         #Ecrire la fonction de calcule des coups légaux grâce au plateau (Board)
-        pass
+        y,x = self.posy, self.posx
+        legalmoves = []
+        move = [(2,1),(2,-1),(-2,1),(-2,-1),(1,2),(1,-2),(-1,2),(-1,-2)]
+
+        for i in move:
+            movey,movex = i
+            if Board[y + movey][x + movey] == None or Board[y + movey][x + movey] == self.Color != self.Color:
+                legalmoves.append(Board[y + movey, x + movex])
+        return legalmoves
+
