a0435d710e459b383c34f539c7ef69e29670eca2
sylvaneaucode <sylvain.craftmine@gmail.com>
2023-09-23 11:32:59 +0200

    Les Légalmoves des tours, et du rois

--- Class/Pieces/Bishop.py
+++ Class/Pieces/Bishop.py
@@ -4,11 +4,12 @@ from Class.Pieces.Piece import *
 class Bishop(Piece):
     """Permet de créer un fou enfant de Piece"""
 
-    def __init__(self, color, x, y):
+    def __init__(self, color):
 
         super().__init__("B", color)
 
     def LegalMoves(self, Board):
+        y,x = self.posy,self.posx
+        legalmoves = []
+    
 
-        #Ecrire la fonction de calcule des coups légaux grâce au plateau (Board)
-        pass
--- Class/Pieces/King.py
+++ Class/Pieces/King.py
@@ -4,11 +4,20 @@ from Class.Pieces.Piece import *
 class King(Piece):
     """Permet de créer un roi enfant de Piece"""
 
-    def __init__(self, color, x, y):
+    def __init__(self, color):
 
-        super().__init__("K", color, x, y)
+        super().__init__("K", color)
 
     def LegalMoves(self, Board):
 
         #Ecrire la fonction de calcule des coups légaux grâce au plateau (Board)
-        pass
+        #penser a vérifier si le roi n'est pas en echec
+        y,x = self.posy,self.posx
+        legalmoves = []
+        move = [(0,1),(0,-1),(1,0),(1,1),(1,-1),(-1,0),(-1,1),(-1,-1)]
+
+        for i in move:
+            movey,movex = move
+            if Board[y + movey][x + movex] == None or Board[y + movey][x + movex] == Piece.Color:
+                legalmoves.append(Board[y + movey][x + movex])
+        return legalmoves
