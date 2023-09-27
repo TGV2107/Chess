a1b5daa3e834a3a9f8c42bd8f01cba345e345adb
sylvaneaucode <sylvain.craftmine@gmail.com>
2023-09-27 08:28:04 +0200

    dsfq

--- Class/Pieces/Rook.py
+++ Class/Pieces/Rook.py
@@ -16,13 +16,13 @@ class Rook(Piece):
         # penser a faire le systeme pour interdir de faire bouger la pièce si on risque d'etre en échec
         for i in range(-7,7):
 
-            if Board[y + i][x] == None or Board[y + i][x] == Piece.Color != self.Color:
+            if Board[y + i][x] == None or Board[y + i][x] and Piece.Color != self.Color:
                 legalmoves.append(Board[y + i][x])
                 if Board[y + i][x] == Piece.Color != self.Color:
                     break
         
         for i in range(-7,7):
-            if Board[y][x + i] == None or Board[y][x + i] == Piece.Color != self.Color:
+            if Board[y][x + i] == None or Board[y][x + i] and Piece.Color != self.Color:
                 legalmoves.append(Board[y][x + i])
                 if Board[y][x + y] == Piece.Color != self.Color:
                     break
