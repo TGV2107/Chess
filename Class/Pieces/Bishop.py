ae6491323e3204393b572f727604a940e1bde2b2
sylvaneaucode <sylvain.craftmine@gmail.com>
2023-09-29 14:27:30 +0200

    bishop et Rook fini il faut encore ajouter echec

--- Class/Pieces/Bishop.py
+++ Class/Pieces/Bishop.py
@@ -11,5 +11,30 @@ class Bishop(Piece):
     def LegalMoves(self, Board):
         y,x = self.posy,self.posx
         legalmoves = []
+        for i in range (7):
+            if Board[y + i][x + i] == None or Board[y + i][x + i] and Piece.Color != self.Color:
+                legalmoves.append(Board[y + i][x + i])
+            if Board[y + i][x + i] == Piece:
+                break
+    
+        for i in range (7):
+            if Board[y + i][x - i] == None or Board[y + i][x + i] and Piece.Color != self.Color:
+                legalmoves.append(Board[y + i][x + i])
+            if Board[y + i][x - i] == Piece:
+                break
+
+        for i in range (7):
+            if Board[y - i][x + i] == None or Board[y + i][x + i] and Piece.Color != self.Color:
+                legalmoves.append(Board[y + i][x + i])
+            if Board[y - i][x + i] == Piece:
+                break
+
+        for i in range (7):
+            if Board[y + i][x - i] == None or Board[y + i][x + i] and Piece.Color != self.Color:
+                legalmoves.append(Board[y + i][x + i])
+            if Board[y + i][x - i] == Piece:
+                break
+
+        return legalmoves
     
 
--- Class/Pieces/Rook.py
+++ Class/Pieces/Rook.py
@@ -14,17 +14,30 @@ class Rook(Piece):
         legalmoves = []
         y,x = self.posy,self.posx
         # penser a faire le systeme pour interdir de faire bouger la pièce si on risque d'etre en échec
-        for i in range(-7,7):
+        for i in range(7):
 
             if Board[y + i][x] == None or Board[y + i][x] and Piece.Color != self.Color:
                 legalmoves.append(Board[y + i][x])
-                if Board[y + i][x] == Piece.Color != self.Color:
+                if Board[y + i][x] == Piece:
                     break
         
-        for i in range(-7,7):
+        for i in range(7):
             if Board[y][x + i] == None or Board[y][x + i] and Piece.Color != self.Color:
                 legalmoves.append(Board[y][x + i])
-                if Board[y][x + y] == Piece.Color != self.Color:
+                if Board[y][x + y] == Piece:
+                    break
+        
+        for i in range(-7,0):
+
+            if Board[y + i][x] == None or Board[y + i][x] and Piece.Color != self.Color:
+                legalmoves.append(Board[y + i][x])
+                if Board[y + i][x] == Piece:
+                    break
+        
+        for i in range(-7,0):
+            if Board[y][x + i] == None or Board[y][x + i] and Piece.Color != self.Color:
+                legalmoves.append(Board[y][x + i])
+                if Board[y][x + y] == Piece:
                     break
 
         return legalmoves
